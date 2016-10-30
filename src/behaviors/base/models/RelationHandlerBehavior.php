8b0a86173c699b87af4c73b28363dc94915a3546
Philip Verbist <philip.verbist@gmail.com>
2016-10-31 00:37:41 +0100

    Streamlined the working of RelationHandlerBehavior

--- src/behaviors/base/models/RelationHandlerBehavior.php
+++ src/behaviors/base/models/RelationHandlerBehavior.php
@@ -10,55 +10,59 @@ use verbi\yii2ExtendedActiveRecord\db\ActiveRecord;
 class RelationHandlerBehavior extends \verbi\yii2Helpers\behaviors\base\Behavior {
     protected $_relations = [];
     
-    protected $relations = [];
-    
-    public $tags;
-    
     public function __get($name) {
-        if(isset($this->relations[$name]) || array_key_exists($name, $this->relations)) {
-            if(isset($this->_relations[$name])) {
-                return $this->_relations[$name];
+        if($this->owner) {
+            $relation = $this->owner->getRelation($name, false);
+            if($relation) {
+                if(isset($this->_relations[$name])) {
+                    return $this->_relations[$name];
+                }
+                return null;
             }
-            return null;
         }
         return parent::__get($name);
     }
     
     public function __set($name, $value) {
-        if(isset($this->relations[$name]) || array_key_exists($name, $this->relations)) {
-            
-            return $this->_relations[$name] = $value;
+        if($this->owner)
+        {
+            $relation = $this->owner->getRelation($name, false);
+            if($relation instanceof \yii\db\ActiveQueryInterface) {
+                if(is_array($value)) {
+                    $relationClassName = $relation->modelClass;
+                    if($relation->multiple) {
+                        $this->_relations[$name] = array_map(
+                            function(&$var) {
+                                if(is_array($var)){
+                                    $relationModel = new $relationClassName();
+                                    $relationModel->setAttributes($var);
+                                    return $relationModel;
+                                }
+                                return $var;
+                            },
+                            $value
+                        );
+                        return;
+                    }
+                    else {
+                        $relationModel = new $relationClassName();
+                        $relationModel->setAttributes($value);
+                        $this->_relations[$name] = $relationModel;
+                        return;
+                    }
+                }
+                $this->_relations[$name] = $value;
+                return;
+            }
         }
         return parent::__set($name, $value);
     }
-    
+
     public function __isset($name) {
         try {
             return $this->__get($name) !== null;
         } catch (\Exception $e) {
             return false;
         }
-    }
-    
-    public function events() {
-        return [
-            ActiveRecord::EVENT_AFTER_SETATTRIBUTES => 'afterSetAttributes',
-        ];
-    }
-    
-    public function afterSetAttributes($event) {
-        // ...
-    }
-    
-    public function attach($owner)
-    {
-        $this->owner = $owner;
-        foreach ($this->events() as $event => $handler) {
-            $owner->on($event, is_string($handler) ? [$this, $handler] : $handler);
-        }
-    }
-    
-    public function setRelations($value) {
-        $this->relations = $value;
     }
 }

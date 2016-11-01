ded1447137584a41890db93a92f940936f4304c4
Philip Verbist <philip.verbist@gmail.com>
2016-11-01 01:59:53 +0100

    Updated relationHandlerBehavior's magic methods to make relation management more simple.

--- src/behaviors/base/models/RelationHandlerBehavior.php
+++ src/behaviors/base/models/RelationHandlerBehavior.php
@@ -1,52 +1,45 @@
 <?php
+
 namespace verbi\yii2Helpers\behaviors\base\models;
+
 use verbi\yii2ExtendedActiveRecord\db\ActiveRecord;
 
-/* 
+/*
  * @author Philip Verbist <philip.verbist@gmail.com>
  * @link https://github.com/verbi/Yii2-Helpers/
  * @license https://opensource.org/licenses/GPL-3.0
-*/
+ */
+
 class RelationHandlerBehavior extends \verbi\yii2Helpers\behaviors\base\Behavior {
     protected $_relations = [];
-    
+
+    public function __call($name, $params) {
+        if ($this->owner && strpos($name, 'set') === 0) {
+            $relation = $this->_getRelation($name);
+            if ($relation) {
+                return $this->_setRelated(lcfirst(substr($name, 3)), array_shift($params));
+            }
+        }
+        return parent::__call($name, params);
+    }
+
     public function __get($name) {
-        if($this->owner) {
-            $relation = $this->owner->getRelation($name, false);
-            if($relation instanceof \yii\db\ActiveQueryInterface) {
-                if(isset($this->_relations[$name])) {
-                    return $this->_relations[$name];
+        if ($this->owner) {
+            $relation = $this->_getRelation($name);
+            if ($relation) {
+                if (isset($this->owner->_relations[$name])) {
+                    return $this->owner->_relations[$name];
                 }
             }
         }
         return parent::__get($name);
     }
-    
+
     public function __set($name, $value) {
-        if($this->owner) {
-            $relation = $this->owner->getRelation($name, false);
-            if($relation instanceof \yii\db\ActiveQueryInterface) {
-                if(is_array($value)) {
-                    $relationClassName = $relation->modelClass;
-                    if($relation->multiple) {
-                        $models = [];
-                        foreach($value as $var) {
-                            $relationModel = new $relationClassName();
-                            $relationModel->setAttributes($var);
-                            $models[] = $relationModel;
-                        }
-                        $this->_relations[$name] = $models;
-                        return;
-                    }
-                    else {
-                        $relationModel = new $relationClassName();
-                        $relationModel->setAttributes($value);
-                        $this->_relations[$name] = $relationModel;
-                        return;
-                    }
-                }
-                $this->_relations[$name] = $value;
-                return;
+        if ($this->owner) {
+            $relation = $this->_getRelation($name);
+            if ($relation) {
+                return $this->_setRelated($name, $value);
             }
         }
         return parent::__set($name, $value);
@@ -58,5 +51,59 @@ class RelationHandlerBehavior extends \verbi\yii2Helpers\behaviors\base\Behavior
         } catch (\Exception $e) {
             return false;
         }
+    }
+
+    protected function _getRelation($name) {
+        if (!array_key_exists($this->_relations[$name])) {
+            $relation = $this->owner->getRelation($name, false);
+            if ($relation instanceof \yii\db\ActiveQueryInterface) {
+                $this->_relations[$name] = $relation;
+            } else {
+                $this->_relations[$name] = null;
+            }
+            return $this->_relations[$name];
+        }
+        return $this->_relations[$name];
+    }
+
+    protected function _setRelated($name, $value) {
+        if ($this->owner) {
+            $relation = $this->_getRelation($name);
+            if ($relation) {
+                if (is_array($value)) {
+                    $relationClassName = $relation->modelClass;
+                    if ($relation->multiple) {
+                        $models = [];
+                        foreach ($value as $var) {
+                            $relationModel = new $relationClassName();
+                            $relationModel->setAttributes($var);
+                            $models[] = $relationModel;
+                        }
+                        $this->owner->_relations[$name] = $models;
+                        return;
+                    } else {
+                        $relationModel = new $relationClassName();
+                        $relationModel->setAttributes($value);
+                        $this->owner->_relations[$name] = $relationModel;
+                        return;
+                    }
+                }
+                $this->owner->_relations[$name] = $value;
+                return;
+            }
+        }
+    }
+    
+    public function hasMethod($name, $checkPublic = false) {
+        if(parent::hasMethod($name, $checkPublic)) {
+            return true;
+        }
+         if (strpos($name,'set')===0 && $this->owner) {
+            $relation = $this->_getRelation(strtolower(substr($name,3)));
+            if ($relation) {
+                return true;
+            }
+        }
+        return false;
     }
 }

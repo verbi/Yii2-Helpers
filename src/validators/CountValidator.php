9a5c6d26b15b7ed0445d7afbc47af657d8fd7f88
Philip Verbist <philip.verbist@gmail.com>
2016-11-02 01:01:15 +0100

    Added some docs

--- src/validators/CountValidator.php
+++ src/validators/CountValidator.php
@@ -1,18 +1,27 @@
 <?php
+
 namespace verbi\yii2Helpers\validators;
+
 use Yii;
 
 /*
  * @author Philip Verbist <philip.verbist@gmail.com>
  * @link https://github.com/verbi/Yii2-Helpers/
  * @license https://opensource.org/licenses/GPL-3.0
-*/
-class CountValidator extends Validator
-{
+ */
+
+class CountValidator extends Validator {
+
+    /**
+     * @var int The minimum number of elements in the collection
+     */
     public $min = 0;
     
+    /**
+     * @var int The maximum number of elements in the collection
+     */
     public $max;
-    
+
     /**
      * @var string the user-defined error message. It may contain the following placeholders which
      * will be replaced accordingly by the validator:
@@ -21,28 +30,28 @@ class CountValidator extends Validator
      * - `{value}`: the value of the attribute being validated
      */
     public $message;
+
     /**
      * @inheritdoc
      */
-    public function init()
-    {
+    public function init() {
         parent::init();
         if ($this->message === null) {
-                $this->message = Yii::t('yii', '{attribute} should have at least {min} and at most {max} elements.');
+            $this->message = Yii::t('yii', '{attribute} should have at least {min} and at most {max} elements.');
         }
     }
-    
+
     /**
      * @inheritdoc
      */
-    protected function validateValue($value)
-    {
-            if(sizeof($value) < $this->min || sizeof($value) > $this->max) {
-                return [$this->message, [
+    protected function validateValue($value) {
+        if (sizeof($value) < $this->min || sizeof($value) > $this->max) {
+            return [$this->message, [
                     'min' => $this->min,
                     'max' => $this->max,
-                ]];
-            }
-            return null;
+            ]];
+        }
+        return null;
     }
+
 }

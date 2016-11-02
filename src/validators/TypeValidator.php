ced1a89e577409f9e8ad6d03bded7a2708ea5e11
Philip Verbist <philip.verbist@gmail.com>
2016-11-02 01:04:09 +0100

    Made TypelValidator work for collections of records

--- src/validators/TypeValidator.php
+++ src/validators/TypeValidator.php
@@ -8,7 +8,16 @@ namespace verbi\yii2Helpers\validators;
 */
 class TypeValidator extends Validator
 {
+    /**
+     * @var string The typename of the value being validated 
+     */
     public $requiredType;
+    
+    /**
+     * @var bool Whether the validator should validate a collection of items
+     */
+    public $multiple = false;
+    
     /**
      * @var string the user-defined error message. It may contain the following placeholders which
      * will be replaced accordingly by the validator:
@@ -18,6 +27,7 @@ class TypeValidator extends Validator
      * - `{requiredType}`: the value of [[requiredType]]
      */
     public $message;
+    
     /**
      * @inheritdoc
      */
@@ -25,8 +35,24 @@ class TypeValidator extends Validator
     {
         parent::init();
         if ($this->message === null) {
-            $this->message = \Yii::t('yii', '{attribute} must be of type "{requiredType}".');
+            $this->message = \Yii::t('yii', '{attribute} must be of type "{requiredType}, but is of type {valueType}".');
         }
+    }
+    
+    /**
+     * Checks whether to validate a collection of items
+     * @return bool
+     */
+    public function getMultiple() {
+        return $this->multiple;
+    }
+    
+    /**
+     * Sets hether to validate a collection of items
+     * @param bool $value
+     */
+    public function setMultiple(bool $value) {
+        $this->multiple = $value;
     }
     
     /**
@@ -36,11 +62,24 @@ class TypeValidator extends Validator
     {
         if ($this->requiredType === null) {
             return null;
-        } elseif (gettype($value) == $this->requiredType || $value instanceof $this->requiredType) {
+        } elseif (!$this->getMultiple()
+                && (gettype($value) == $this->requiredType || $value instanceof $this->requiredType)) {
+            return null;
+        }
+        elseif ($this->getMultiple() && (is_array($value) || $value instanceof \Traversable)) {
+            foreach($value as $item) {
+                if(gettype($item) != $this->requiredType && !$item instanceof $this->requiredType) {
+                    return [$this->message, [
+                        'requiredType' => $this->requiredType,
+                        'valueType' => gettype($item),
+                    ]];
+                }
+            }
             return null;
         }
         return [$this->message, [
             'requiredType' => $this->requiredType,
+            'valueType' => gettype($value),
         ]];
     }
 }

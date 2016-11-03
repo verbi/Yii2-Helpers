e19a863ae4722fec1701c6d6fb82650a9ee3a21b
Philip Verbist <philip.verbist@gmail.com>
2016-11-03 03:47:04 +0100

    Simplified ModelValidator

--- src/validators/ModelValidator.php
+++ src/validators/ModelValidator.php
@@ -12,11 +12,6 @@ use verbi\yii2Helpers\Html;
 class ModelValidator extends Validator
 {
     /**
-     * @var bool Whether the validator should validate a collection of items
-     */
-    public $multiple = false;
-    
-    /**
      * @var string the user-defined error message. It may contain the following placeholders which
      * will be replaced accordingly by the validator:
      *
@@ -31,29 +26,8 @@ class ModelValidator extends Validator
     {
         parent::init();
         if ($this->message === null) {
-            if(!$this->getMultiple()) {
-                $this->message = Yii::t('yii', '{attribute} is invalid.');
-            }
-            else {
-                $this->message = Yii::t('yii', 'One of the values of {attribute} is invalid.');
-            }
+            $this->message = Yii::t('yii', '{attribute} is invalid.');
         }
-    }
-    
-    /**
-     * Checks whether to validate a collection of items
-     * @return bool
-     */
-    public function getMultiple() {
-        return $this->multiple;
-    }
-    
-    /**
-     * Sets hether to validate a collection of items
-     * @param bool $value
-     */
-    public function setMultiple(bool $value) {
-        $this->multiple = $value;
     }
     
     /**
@@ -61,21 +35,10 @@ class ModelValidator extends Validator
      */
     protected function validateValue($value)
     {
-        if (!$this->getMultiple() && $value instanceof Model && $value->validate()) {
+        if ($value instanceof Model && $value->validate()) {
             return null;
         }
-        elseif($this->getMultiple() &&(is_array($value) || $value instanceof \Traversable)) {
-            foreach($value as $item) {
-                if(!$item instanceof Model || !$item->validate()) {
-                    //return [Html::errorSummary($value),[]];
-                    return [$this->message, [
-                    ]];
-                }
-            }
-            return null;
-        }
-        return [$this->message, [
-            //'requiredValue' => $this->requiredValue,
-        ]];
+        
+        return [$this->message, []];
     }
 }

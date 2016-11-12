ec5950629e6a608dab7dc46e989aac4a26cbfe7a
Philip Verbist <philip.verbist@gmail.com>
2016-11-12 01:48:40 +0100

    General function event params

--- src/events/GeneralFunctionEvent.php
+++ src/events/GeneralFunctionEvent.php
@@ -1,6 +1,29 @@
 <?php
 namespace verbi\yii2Helpers\events;
 
-class GenearalFunctionEvent extends \yii\base\Event {
-    public $returnValue;
+class GeneralFunctionEvent extends \yii\base\Event {
+    protected $returnValue;
+    protected $hasReturnValue = false;
+    protected $params;
+    
+    public function setReturnValue($value) {
+        $this->returnValue = $value;
+        $this->hasReturnValue = true;
+    }
+    
+    public function getParams() {
+        return $this->params;
+    }
+    
+    public function setParams($params) {
+        $this->params = $params;
+    }
+    
+    public function getReturnValue() {
+        return $this->returnValue;
+    }
+    
+    public function hasReturnValue() {
+        return $this->hasReturnValue;
+    }
 }

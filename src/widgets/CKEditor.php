b5a82415c2c7262d5e7b7fe2a7cb6c48d04f4d72
Philip Verbist <philip.verbist@gmail.com>
2017-03-31 22:32:36 +0200

    Added ckeditorwidgetAsset

---
+++ src/widgets/CKEditor.php
@@ -0,0 +1,37 @@
+<?php
+namespace verbi\yii2Helpers\widgets;
+
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+use verbi\yii2Helpers\widgets\assets\CKEditorWidgetAsset;
+
+class CKEditor extends \dosamigos\ckeditor\CKEditor {
+    /**
+     * Registers CKEditor plugin
+     * @codeCoverageIgnore
+     */
+    protected function registerPlugin()
+    {
+        $js = [];
+
+        $view = $this->getView();
+
+        CKEditorWidgetAsset::register($view);
+
+        $id = $this->options['id'];
+
+        $options = $this->clientOptions !== false && !empty($this->clientOptions)
+            ? Json::encode($this->clientOptions)
+            : '{}';
+
+        $js[] = "CKEDITOR.replace('$id', $options);";
+        $js[] = "dosamigos.ckEditorWidget.registerOnChangeHandler('$id');";
+
+        if (isset($this->clientOptions['filebrowserUploadUrl']) || isset($this->clientOptions['filebrowserImageUploadUrl'])) {
+            $js[] = "dosamigos.ckEditorWidget.registerCsrfImageUploadHandler();";
+        }
+
+        $view->registerJs(implode("\n", $js));
+    }
+}
